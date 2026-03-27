8e73422c7beaae50d4bb4f744bd661f310e48655
JoaoSandovall <jpsandovalveras@gmail.com>
2026-03-26 22:00:23 -0300

    fix: removendo mission

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { Hero } from './components/Hero';
 import { DonationInfo } from './components/DonationInfo';
-import { Mission } from './components/Mission';
 import { FairLocation } from './components/FairLocation';
 import { LocationMap } from './components/LocationMap';
 import { ProductShowcase } from './components/ProductShowcase';
@@ -11,7 +10,6 @@ export default function App() {
     <div className="min-h-screen">
       <Hero />
       <FairLocation />
-      <Mission /> 
       <DonationInfo />
       <LocationMap />
       <Footer />
